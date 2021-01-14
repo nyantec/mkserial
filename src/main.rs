706677d71c61f2c05cb74ef132d69dc0280201a3
Mikael Voss <mvs@nyantec.com>
2021-01-15 00:11:37 +0100

    Update code to support new dependency versions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 extern crate rand;
 extern crate num_bigint as bigint;
 
-use rand::OsRng;
+use rand::rngs::OsRng;
 use bigint::RandBigInt;
 
 fn main() {
-	let mut rng = OsRng::new().expect("Failed to create RNG");
-
-	println!("{}", rng.gen_biguint(159));
+	println!("{}", OsRng.gen_biguint(159));
 }

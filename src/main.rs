8920e0d8422373de858a9a8c82c3e936c53f2eca
Mikael Voss <mvs@nyantec.com>
2021-11-27 21:09:15 +0100

    Remove unnecessary extern crate declarations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-extern crate rand;
-extern crate num_bigint as bigint;
-
 use rand::rngs::OsRng;
-use bigint::RandBigInt;
+use num_bigint::RandBigInt;
 
 fn main() {
 	println!("{}", OsRng.gen_biguint(159));
